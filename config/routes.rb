40c5e89cbb617a8280f9354fe81fa814e70e4916
kei-asari <kei.asari.93.s@gmail.com>
2016-01-27 16:33:02 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :categories, only: [:index, :show]
   resources :clips, only: [:create, :destroy]
-  resources :userss, only: [:show, :posts_index]
+  resources :users, only: [:show, :posts_index]
 
 
   devise_for :users
