5ca968c13b02e40672a283ceffe2c14534ba01f0
kei-asari <kei.asari.93.s@gmail.com>
2016-02-05 17:40:58 +0900

    カラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127072155) do
+ActiveRecord::Schema.define(version: 20160205083911) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",       limit: 255
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20160127072155) do
     t.integer  "user_id",     limit: 4
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.string   "thumbnail",   limit: 255
   end
 
   create_table "categories", force: :cascade do |t|
