3328399f481c701ca651cdfed0f41b950beb5fa8
kei-asari <kei.asari.93.s@gmail.com>
2016-02-16 23:51:20 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
 
-  get 'users/show'
-
   get 'users/posts_index'
 
   root 'home#index'
 
+  devise_for :users
   resources :home, only: :index
   resources :articles, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :categories, only: [:index, :show]
   resources :clips, only: [:create, :destroy]
   resources :users, only: [:show, :posts_index]
-
-
-  devise_for :users
 
 end
