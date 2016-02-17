2ed752a7031f45709001eb2ea331096b51e0c031
kei-asari <kei.asari.93.s@gmail.com>
2016-02-17 11:45:58 +0900

    リダイレクト

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,14 @@
 class HomeController < ApplicationController
+
+   before_action :move_to_index, except: :index
+
   def index
     @articles = Article.all.order("id DESC")
   end
+
+  private
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
 end
