0e5ed08f863a3a4efd41790f156290e1c9f5b67b
kei-asari <kei.asari.93.s@gmail.com>
2016-02-17 19:26:13 +0900

    ユーザー画面のビュー周り

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @articles = @user.articles
   end
 
   def posts_index
+    @user = User.find(params[:id])
+    @posts = @user.articles
   end
 end
