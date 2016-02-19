c19d402aec418ca70750a5bef046bc879aa20a9e
kei-asari <kei.asari.93.s@gmail.com>
2016-02-19 15:23:05 +0900

    article&clip

--- app/controllers/clips_controller.rb
+++ app/controllers/clips_controller.rb
@@ -1,7 +1,14 @@
 class ClipsController < ApplicationController
   def create
+    @clip = Clip.create(create_params)
+    redirect_to controller: :article, action: :show
   end
 
   def destroy
+  end
+
+  private
+  def create_params
+    params.merge(article_id: params[:article_id], user_id: current_user.id)
   end
 end
